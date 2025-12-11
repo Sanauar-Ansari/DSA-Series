22db8d60e5d622b63592ac3e43a236498a695cda
Sanauar Ansari <sanauaransari99@gmail.com>
2025-12-11 17:14:38 +0530

    largest element from array

--- Arrays/Array.java
+++ Arrays/Array.java
@@ -24,11 +24,46 @@ public class Array {
           printArray(arr);
     }
 
+    // Q1. Print only -ve value
+    public static void printNegativeValue(){
+         int[] arr={1,-2,3,-4,5,-6,7,-8};
+         for(int i=0;i<arr.length;i++){
+           if(arr[i]<0){
+            System.out.println(arr[i]);
+           }
+    }
+    }
+   // Q2. Print sum of array
+        public static int sumOfTheArray(int [] arr){
+        int n=arr.length;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum=sum+arr[i];
+        } 
+        return sum;
+    }
+
+       // Q3. Print sum of array
+        public static int lasrgestElement(int [] arr){
+        int n=arr.length;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        } 
+        return max;
+    }
+
 
 public static void main(String[] args) {
-    // int arr[]={1,2,3,4,5,6};
+    int arr[]={1,2,3,4,50,6,-21};
     // printArray(arr);
     // takeInput();
+
+   
+ int sum= lasrgestElement(arr);
+ System.out.println(sum);
    
 
 
